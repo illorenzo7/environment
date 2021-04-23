e957dc50c2bf0fe57e3db8ec5c4254fe9afee86e
Loren <loren.matilsky@gmail.com>
2021-04-23 16:06:47 -0700

    updating some directory changes in bashrc_ra_routines

--- python_startup.py
+++ python_startup.py
@@ -4,9 +4,10 @@ import sys, os
 sys.path.append(os.environ['rapp'])
 sys.path.append(os.environ['raco'])
 sys.path.append(os.environ['rapl'])
+sys.path.append(os.environ['rapl'] + '/azav')
 from rayleigh_diagnostics import *
 from common import *
-from plotcommon import testtex
+from plotcommon import *
 from azav_util import plot_azav
 from reference_tools import equation_coefficients
 
